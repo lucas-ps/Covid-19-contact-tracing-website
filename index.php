9b065420f57a76674fef60a13444126156a275d0
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-04-08 14:56:40 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-// Log out user and send them to login page
-if(session_status() === PHP_SESSION_ACTIVE){
-    session_destroy();
-}
-$content = file_get_contents('login.php');
-echo $content;
+
+header('Location: ./login.php');
+
